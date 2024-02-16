aa3c2e60837b9d42a250686dc2dab1c7d0d18a6c
Ivan Riazanov <omega19961@gmail.com>
2024-02-16 18:07:50 +0600

    changed public and private order

--- include/Menu.h
+++ include/Menu.h
@@ -7,27 +7,6 @@
 #include "MenuIcons.h"
 class Menu
 {
-public:
-    void show_page();
-
-    void show_main_page();
-
-    void show_play_page();
-
-    void show_settings_page();
-
-    void show_calculate_page();
-    void next_item();
-
-    void prev_item();
-
-    int select_item();
-
-    void show_wifi_page();
-
-    void update_status(bool wifi, bool ogs, int battery);
-
-    void init();
 
 private:
     int _current_item;
@@ -52,6 +31,28 @@ private:
     void _page_display(int icons[], char names[][20]);
 
     void _show_status();
+
+public:
+    void show_page();
+
+    void show_main_page();
+
+    void show_play_page();
+
+    void show_settings_page();
+
+    void show_calculate_page();
+    void next_item();
+
+    void prev_item();
+
+    int select_item();
+
+    void show_wifi_page();
+
+    void update_status(bool wifi, bool ogs, int battery);
+
+    void init();
 };
 
 extern Adafruit_SSD1306 display;
