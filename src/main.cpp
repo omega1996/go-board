33017862072ea18adcc682e0860e96fd0a711a29
Ivan Riazanov <omega19961@gmail.com>
2024-03-18 20:37:02 +0600

    state reading function

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,33 @@
 #include "GoMenu.h"
 #include "GyverButton.h"
 
+#include <CD74HC4067.h>
+#include "FastLED.h"
+
 #define DOWN_BTN_PIN 1
 #define OK_BTN_PIN 3
 #define UP_BTN_PIN 2
+
+// Светодиоды
+#define NUM_LEDS 81 // кол-во светодиодов
+#define DATA_PIN 14 // Пин светодиодной ленты
+
+// мультиплексоры
+
+const int s0InputPin = 5;    // Пин S0 мультиплексора
+const int s1InputPin = 7;    // Пин S0 мультиплексора
+const int s2InputPin = 9;    // Пин S0 мультиплексора
+const int s3InputPin = 11;   // Пин S0 мультиплексора
+const int analogMuxPin = 12; // 12 пин это ADC2_1, ближайший 10 пин ADC1_9
+
+CD74HC4067 input_mux(s0InputPin, s1InputPin, s2InputPin, s3InputPin);
+
+// регистры сдвига
+
+const int latchPin = 40;
+const int clockPin = 39;
+const int dataPin = 15;
+// кнопки
 
 GButton downButton(DOWN_BTN_PIN);
 GButton okButton(OK_BTN_PIN);
@@ -16,16 +40,36 @@ GButton upButton(UP_BTN_PIN);
 
 bool menuLocked = false;
 
-bool set_rules(Adafruit_SSD1306 *display)
+void readState()
 {
-
-  auto myPrevCallback = []()
+  for (int i = 0; i < 9; ++i)
   {
-    // menuLocked = false;
-  };
-  manager.setSelectCallback(myPrevCallback);
-  return false;
-}
+    input_mux.channel(i);
+    short state = 1;
+
+    for (int j = 0; j < 9; ++j)
+    {
+      digitalWrite(latchPin, LOW);
+      shiftOut(dataPin, clockPin, MSBFIRST, state >> 8);   // 2 регистр
+      shiftOut(dataPin, clockPin, MSBFIRST, state & 0xff); // 1 регистр
+      digitalWrite(latchPin, HIGH);
+      delay(1);
+      state = state << 1;
+      int currentSensorValue = analogRead(analogMuxPin);
+
+      int ledIndex = i * 9 + j;
+
+      if (currentSensorValue > 100)
+      {
+
+        Serial.print(">");
+        Serial.print(ledIndex);
+        Serial.print(":");
+        Serial.println(currentSensorValue);
+      }
+    }
+  }
+};
 
 void setup()
 {
@@ -52,17 +96,28 @@ void setup()
     startTime = millis();
     portalRunning = true;
   }
-
+  // menu
   MenuItem root = createRootMenu();
   menu.init(&root);
 
   menu.showPage();
+
+  // shift reg
+  // Setup pins as Outputs
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
+  // mux
+  pinMode(analogMuxPin, INPUT);
 
   Serial.println("Setup done");
 }
 
 void loop()
 {
+
+  readState();
   // Serial.println("[APP] Free memory: " + String(esp_get_free_heap_size()) + " bytes");
   connectWiFi();
   menu.update_status(wifi_connected, false, 1);
